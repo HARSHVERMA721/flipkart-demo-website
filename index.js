84ead6edbdd149fb5138794b68068954af8ed24e
HARSHVERMA721 <hv0096@gmail.com>
2025-12-30 14:22:41 +0530

    new changes

---
+++ index.js
@@ -0,0 +1,10 @@
+let prevbtn = document.querySelector(".prev");
+let nextbtn = document.querySelector(".next");
+
+prevbtn.addEventListener("click", () => {
+  console.log("prev");
+});
+
+nextbtn.addEventListener("click", () => {
+  console.log("next");
+});
